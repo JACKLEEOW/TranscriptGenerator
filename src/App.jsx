bd37ca72967231d77f2a3e2e72c8acee9ebeb78d
Jack Le <jack.le.031231@hotmail.com>
2025-07-10 14:28:44 -0600

    Header start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
+import Header from './Header.jsx'
 
 function App() {
 
-  return (
-    <>
-    </>
+  return ( 
+    <Header/>
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    
+    
+    return (
+        <h1>Generate a Transcript</h1>
+
+    );
+
+
+
+}
+export default Header;
