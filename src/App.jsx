38cca539a0bcd52907c2865c021964901f089872
tai-shis <questai.35@gmail.com>
2025-07-19 15:57:55 -0600

    adding new requirements/packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import ClickMeButton from './components/ClickMeButton.jsx'
+import FileForm from './components/FileForm.jsx'
 function App() {
 
   return ( 
     <div className='bg-site h-screen'>
         <Header/>
-        <ClickMeButton/>
+        {/* <ClickMeButton/> */}
+        <FileForm />
     </div>
   )
 }
---
+++ src/components/FileForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { useDropzone } from 'react-dropzone';
+
+function FileForm() {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default FileForm;
